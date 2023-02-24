e488a38f720e8e8f9a816aedaf51edc8b5c5ecf3
Satori32 <Huga8989+githubf@gmail.com>
2023-02-24 21:57:27 +0900

    2nd. it at useless. dont touch.

--- ソース.cpp
+++ ソース.cpp
@@ -15,17 +15,19 @@
 #pragma comment(lib,"ws2_32.lib")
 #pragma warning(disable : 4996)
 
+//this code is now incomplete,dont touch.
+
 class WinSockCaller {
 public:
 	int Call() {
 		return WSAStartup(MAKEWORD(2, 2), &WD);
 	}
-	int Close() {
-		return WSACleanup();
-	}
-
 	virtual ~WinSockCaller() {
 		Close();
+	}
+
+	int Close() {
+		return WSACleanup();
 	}
 
 	WSADATA GetData() {
@@ -40,35 +42,71 @@ protected:
 class TCP_Client {
 public:
 	TCP_Client() {}
-	TCP_Client(const char* IP, const std::uint16_t& Port) { Connect(IP, Port); }
+	TCP_Client(const char* hostname, const std::uint16_t& Port) { Connect(hostname, Port); }
 	virtual ~TCP_Client() {
 		if (S != INVALID_SOCKET) { DisConnect(); }
 	}
 
-	SOCKET MakeSocket() {
-		S = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	SOCKET Connect(const char* HOSTNAME, const std::uint16_t& Port) {
+		ADDRINFO AF = { 0, };
 
-		if (S == INVALID_SOCKET) { return INVALID_SOCKET; }
-		return S;
+		//AF.ai_family = AF_UNSPEC;
+		AF.ai_family = AF_INET;
+		AF.ai_socktype = SOCK_STREAM;
+		AF.ai_protocol = IPPROTO_TCP;
+
+		ADDRINFO* AI = nullptr;
+		std::string P = std::to_string(Port);
+
+		if (getaddrinfo(HOSTNAME, P.c_str(), &AF, &AI) != 0) { return INVALID_SOCKET; }
+
+		SOCKET Z = INVALID_SOCKET;
+
+		for (addrinfo* X = AI; X; X = X->ai_next) {
+			Z = socket(X->ai_family, X->ai_socktype, X->ai_protocol);
+			if (Z == INVALID_SOCKET) { freeaddrinfo(AI); return Z; }
+			if (bind(Z, X->ai_addr, X->ai_addrlen) == SOCKET_ERROR) {
+				closesocket(Z);
+				Z = INVALID_SOCKET;
+				continue;
+			}
+			if (connect(Z, X->ai_addr, X->ai_addrlen) == SOCKET_ERROR) {
+				closesocket(Z);
+				Z = INVALID_SOCKET;
+				continue;
+			}
+			break;
+		}
+		freeaddrinfo(AI);
+		S = Z;
+		return Z;
 	}
-
+	/** /
 	int Connect(const char* IP, std::uint16_t Port) {//IPv4 cliant??
 		A.sin_addr.S_un.S_addr = inet_addr(IP);
 		A.sin_family = AF_INET;
 		A.sin_port = htons(Port);
 		return  connect(S, (SOCKADDR*)&A, sizeof(A));
 	}
-
+	int Connect(const SOCKADDR_IN& In) {//IPv4 cliant??
+		int X = connect(S, (SOCKADDR*)&In, sizeof(In));
+		if (X == SOCKET_ERROR) { if (LastError() != WSAEWOULDBLOCK) { return SOCKET_ERROR; } }
+		A = In;
+		return X;
+	}
+	/**/
 	int Write(const std::vector<char>& In) {
-		return sendto(S, In.data(), In.size(), 0, (SOCKADDR*)&A, sizeof(A));
+		return send(S, In.data(), In.size(), 0);
+		//return send(S, In.data(), In.size(), 0, (SOCKADDR*)&A, sizeof(A));
 	}
 	int Write(const char* In, std::size_t L) {
-		return sendto(S, In, L, 0, (SOCKADDR*)&A, sizeof(A));
+		return send(S, In, L, 0 );
+		//return send(S, In, L, 0, (SOCKADDR*)&A, sizeof(A));
 	}
 
 	std::vector<char> Read() {
 
-		static const int L = 65530;
+		static const int L = 65000;
 		char D[L] = { 0, };
 
 		int S = sizeof(A);
@@ -77,22 +115,35 @@ public:
 
 		std::vector<char> RR;
 
-		while (R = recvfrom(S, D, L, 0, (SOCKADDR*)&A, &S)) {
-			RR.insert(RR.end(), D, D+min(L,R));//min provide by windows. yes i know STL Have too.but it is confrict. 
+		while (R = recv(S, D, L, 0)) {
+		//while (R = recvfrom(S, D, L, 0, (SOCKADDR*)&A, &S)) {
+			RR.insert(RR.end(), D, D+R);//min provide by windows. yes i know STL Have too.but it is confrict. 
 		}
 
 		return RR;
 	}
 	int Read(char* Buf,std::size_t L) {
 		int S = sizeof(A);
-		return recvfrom(S, Buf, L, 0, (SOCKADDR*)&A, &S);
+		return recv(S, Buf, L, 0);
+		//return recvfrom(S, Buf, L, 0, (SOCKADDR*)&A, &S);
 	}
 
 	int DisConnect() {//after all conection end. to must call WSAcleanup(). but my case to  auto absove by [WinSockCaller] too it.
+		
+		if (S == INVALID_SOCKET) { return SOCKET_ERROR; }
+
 		auto X = S;
 		S = INVALID_SOCKET;
 		A = SOCKADDR_IN();
 		return closesocket(X);
+	}
+
+	int LastError() {
+		return WSAGetLastError();
+	}
+
+	int FormatError() {
+		return FormatError();
 	}
 
 protected:
@@ -100,21 +151,52 @@ protected:
 	SOCKADDR_IN A = { 0, };
 };
 
+unsigned long IPByNumber(unsigned char A, unsigned char B, unsigned char C, unsigned char D) {
+	return D << 24 | C << 16 | B << 8 | A;
+}
+
+SOCKADDR_IN MakeSOCKADDER(short F, unsigned char A, unsigned char B, unsigned char C, unsigned char D, unsigned short Port) {
+	SOCKADDR_IN R;
+	R.sin_family = F;
+	R.sin_port = htons(Port);
+	R.sin_addr.S_un.S_addr = IPByNumber(A, B, C, D);
+
+	return R;
+}
 
 
 int main() {
 	WinSockCaller WS;
-	WS.Call();
+	if (WS.Call() != 0) { return -1; }
 
-	const char* HN = "www.yahoo.:co.jp";
+	const char* HN = "www.google.co.jp";
+/** /
 	LPHOSTENT LHT = gethostbyname(HN);
 
-	/** /
+	SOCKADDR_IN Q = MakeSOCKADDER(AF_INET,(std::uint8_t)(*LHT->h_addr_list)[0],(std::uint8_t)(*LHT->h_addr_list)[1],(std::uint8_t)(*LHT->h_addr_list)[2],(std::uint8_t)(*LHT->h_addr_list)[3],27915);
 
+	const int L = 1128;
+	char A[L] = { 0, };
+	char B[L] = { 0, };
+	getnameinfo((SOCKADDR*) & Q, sizeof(Q), A, L,B,L,0);
+
+	LHT = gethostbyname(A);
+	Q = MakeSOCKADDER(AF_INET,(std::uint8_t)(*LHT->h_addr_list)[0],(std::uint8_t)(*LHT->h_addr_list)[1],(std::uint8_t)(*LHT->h_addr_list)[2],(std::uint8_t)(*LHT->h_addr_list)[3],27915);
+
+/**/
+	
 	TCP_Client TC;
-	TC.MakeSocket();
-	TC.Connect("127.0,0,1", 27015);
-	/**/
+	if (TC.Connect(HN, 27015) == INVALID_SOCKET) { std::cout << TC.LastError() << std::endl; return -1; }
+	//if(TC.Connect(Q) == SOCKET_ERROR) { std::cout << TC.LastError() << std::endl; return -1; }
+
+	std::this_thread::sleep_for(std::chrono::seconds(4));
+
+	auto R = TC.Read();
+	auto W = TC.Write("DISCONNECT!", sizeof("DISCONNECT!"));
+
+
+	TC.DisConnect();
+
 }
 
 /** /
